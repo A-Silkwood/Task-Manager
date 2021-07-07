5e92c54cc07372e501ef35ade012c00acc1fe715
Andrew Silkwood <silkwood.andrew@gmail.com>
2021-07-07 04:39:25 -0700

    legend created

--- src/components/App.js
+++ src/components/App.js
@@ -12,12 +12,29 @@ function App() {
   const [showCreateForm, setShowCreateForm] = useState(false);
   const toggleCreateForm = () => { setShowCreateForm(!showCreateForm) };
 
+  const getStatusColor= (status) => {
+    switch(status) {
+      case 'incomplete':
+          return '#e07b00';
+      case 'delayed':
+          return '#00e0dc';
+      case 'late':
+          return '#ae00ff';
+      case 'complete':
+          return '#00e00b';
+      case 'cancelled':
+          return '#ff0000';
+      default:
+          return 'white';
+    }
+  };
+
   return (
     <div className="App">
       <h1 className="App-header">Task Manager</h1>
       <Manager onClick={ toggleCreateForm } isClicked={ showCreateForm } />
       {showCreateForm && <Form onClose={ toggleCreateForm }/>}
-      <Legend/>
+      <Legend getStatusColor={ getStatusColor }/>
       <TaskList/>
     </div>
   );
--- src/components/Legend.js
+++ src/components/Legend.js
@@ -1,7 +1,38 @@
-const Legend = () => {
+import '../css/Legend.css';
+
+const Legend = ({ getStatusColor }) => {
     return (
-        <div>
-            
+        <div className='Legend'>
+            <div className='Legend-key-cont'>
+                <div className='Legend-key' id='incomplete'
+                    style={{ backgroundColor:getStatusColor('incomplete') }}>
+                    <label className='lbl' htmlFor='incomplete'>Incomplete</label>
+                </div>
+            </div>
+            <div className='Legend-key-cont'>
+                <div className='Legend-key' id='late'
+                    style={{ backgroundColor:getStatusColor('late') }}>
+                    <label className='lbl' htmlFor='late'>Late</label>
+                </div>
+            </div>
+            <div className='Legend-key-cont'>
+                <div className='Legend-key' id='delayed'
+                    style={{ backgroundColor:getStatusColor('delayed') }}>
+                    <label className='lbl' htmlFor='delayed'>Delayed</label>
+                </div>
+            </div>
+            <div className='Legend-key-cont'>
+                <div className='Legend-key' id='complete'
+                    style={{ backgroundColor:getStatusColor('complete') }}>
+                    <label className='lbl' htmlFor='complete'>Complete</label>
+                </div>
+            </div>
+            <div className='Legend-key-cont'>
+                <div className='Legend-key' id='cancelled'
+                    style={{ backgroundColor:getStatusColor('cancelled') }}>
+                    <label className='lbl' htmlFor='cancelled'>Cancelled</label>
+                </div>
+            </div>
         </div>
     )
 }
